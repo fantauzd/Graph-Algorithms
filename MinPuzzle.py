dd0e0f2d6f9aa315435b8557dd979e454d9296a2
fantauzd <fantauzd@oregonstate.edu>
2024-05-18 23:43:14 -0400

    defined problem

--- MinPuzzle.py
+++ MinPuzzle.py
@@ -1,1 +1,9 @@
 # Dominic Fantauzzo
+
+def minEffort(puzzle):
+    """
+    Solves the min effort puzzle problem listed above.
+    :param puzzle: a 3d puzzle in the form of a 2d array with height values in the cells.
+    like puzzle[][] = [[1, 3, 5], [2, 8, 3], [3, 4, 5]]
+    :return: The minimum possible effort to solve the given puzzle.
+    """
