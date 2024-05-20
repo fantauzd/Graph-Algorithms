1a60154abf32d74c2c840f4f5eee1f3e615f8b2f
fantauzd <fantauzd@oregonstate.edu>
2024-05-20 00:54:53 -0400

    added tests for dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -24,3 +24,15 @@ def calculate_distances(graph, starting_vertex):
                 heapq.heappush(pq, (distance, neighbor))
 
     return distances
+
+if __name__ == "__main__":
+    example_graph = {
+        'U': {'V': 2, 'W': 5, 'X': 1},
+        'V': {'U': 2, 'X': 2, 'W': 3},
+        'W': {'V': 3, 'U': 5, 'X': 3, 'Y': 1, 'Z': 5},
+        'X': {'U': 1, 'V': 2, 'W': 3, 'Y': 1},
+        'Y': {'X': 1, 'W': 1, 'Z': 1},
+        'Z': {'W': 5, 'Y': 1},
+    }
+
+    print(calculate_distances(example_graph, 'X'))
