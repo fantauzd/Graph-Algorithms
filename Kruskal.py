0215564cd4519c94002b3aa584b7346298e44a9a
fantauzd <fantauzd@oregonstate.edu>
2024-05-26 13:37:50 -0400

    added kruskals with a class for disjoint sets

---
+++ Kruskal.py
@@ -0,0 +1,76 @@
+# Implement Kruskal's algorithm to find minimum spanning tree
+''''
+source: https://www.geeksforgeeks.org/disjoint-set-data-structures/
+'''
+#Pseudocode
+
+# def Kruskal(V,E):
+#     sort E by increasing weight
+#
+#     for v in V:
+#         create a tree for each v
+#
+#     MST = {}
+#
+#     for i in Range(|E|):
+#         (u,v) <- lightest edge in E
+#
+#         if u and v not in same tree:
+#             MST.add((u,v))
+#             merge u and v trees
+#
+#     return MST
+
+class DisjSet:
+    def __init__(self, n):
+        # Constructor to create and
+        # initialize sets of n items
+        self.rank = [1] * n
+        self.parent = [i for i in range(n)]
+
+        # Finds set of given item x
+
+    def find(self, x):
+
+        # Finds the representative of the set
+        # that x is an element of
+        if (self.parent[x] != x):
+            # if x is not the parent of itself
+            # Then x is not the representative of
+            # its set,
+            self.parent[x] = self.find(self.parent[x])
+
+            # so we recursively call Find on its parent
+            # and move i's node directly under the
+            # representative of this set
+
+        return self.parent[x]
+
+        # Do union of two sets represented
+
+    # by x and y.
+    def Union(self, x, y):
+
+        # Find current sets of x and y
+        xset = self.find(x)
+        yset = self.find(y)
+
+        # If they are already in same set
+        if xset == yset:
+            return
+
+        # Put smaller ranked item under
+        # bigger ranked item if ranks are
+        # different
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+
+            # If ranks are same, then move y under
+        # x (doesn't matter which one goes where)
+        # and increment rank of x's tree
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] = self.rank[xset] + 1
